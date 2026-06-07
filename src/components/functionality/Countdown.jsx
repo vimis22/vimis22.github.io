1aed1cb1c41cf923feab0fe8cd54c2773f7ea5ce
vimis22 <vimis22@student.sdu.dk>
2026-06-07 16:01:27 +0200

    Updates

--- src/components/functionality/Countdown.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import useCountdown from '../../hooks/useCountdown.js'
-
-export default function Countdown({ deadline = '2026-06-01' }) {
-  const days = useCountdown(deadline)
-  return (
-    <div id="countdown">{days !== null ? `Days remaining: ${days}` : ''}</div>
-  )
-}
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // For user.github.io root pages, base should be '/'
-  base: '/',
-})
